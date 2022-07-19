94b24de69c56a3f7f4a9cf29d080fc4e0f421eb0
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-19 14:45:48 -0400

    added loading function

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ const quoteText = document.getElementById("quote");
 const quoteAuthor = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+//  -- LOADER --
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 //  --  API  --
 let apiQuotes = [];
 
 // Show New quote
 function newQuote() {
+  loading();
   //  Pick a random quote from apiQuotes array or localQuotes and comment out the rest of the file
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if author field is blank and repalce it with 'unknown'
@@ -25,11 +40,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  //   Set Quote, Hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 //  Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiURL = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiURL);
@@ -40,14 +58,14 @@ async function getQuotes() {
   }
 }
 
-// -- TWITTER BUTTON --
+// -- BUTTONS --
 // Tweet a Quote
 function tweetQuote() {
   const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
   window.open(twitterURL, "_blank");
 }
 
-// Add event listener on button
+// Add event listener on buttons
 newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 
